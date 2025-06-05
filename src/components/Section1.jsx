7badbbdc11e4f68de7bfce7268511766f362c537
Chibuike Nnorom <nnoromchibuikepromise@gmail.com>
2025-06-05 12:47:02 +0100

    new

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Section1 = () => {
   return (
-    <div>Section1</div>
+    <div>
+        <h1>Section 1</h1>
+        <p>This is the first section of the application.</p>
+        <p>It contains introductory content and sets the stage for the rest of the app.</p>
+        <button onClick={() => alert('Learn more clicked!')}>Learn More</button>
+    </div>
   )
 }
 
